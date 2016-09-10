c4a931988b54f85ea292ca1a2c85f7fd52cffdee
sumesh95 <sumeshm007@gmail.com>
2016-09-10 12:17:15 +0530

    Update and rename pangram12.java to pan.java

--- pan.java
+++ pan.java
@@ -1,10 +1,6 @@
 package pro;
-
-
 import java.util.Scanner;
-
 public class PangramChecker {
-
 	public static void main(String[] args) {
 		char[] alpha = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
 				'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
